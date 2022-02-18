b568c2e7a16616532c98c07cbe6ba2d593bd4170
Egidijus Git <egidijuslenk@gmail.com>
2022-02-18 19:02:54 +0200

    update

--- crud.py
+++ crud.py
@@ -1,17 +1,36 @@
-from typing import Any, Dict, Optional, Union
-
-from fastapi import Depends, HTTPException
-from pydantic import BaseModel
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlalchemy.orm import Session
 
 from security import get_password_hash, verify_password
+import models, schemas
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
 
 
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
 
 
-class CrudUser(BaseModel):
-    def __init__(__pydantic_self__, **data: Any) -> None:
-        super().__init__(**data)
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
 
-    def create(self, db: Session, )
-    
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password + "notreallyhashed"
+    db_user = models.User(user=user.user, email=user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_items(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Content).offset(skip).limit(limit).all()
+
+
+def create_user_item(db: Session, item: schemas.ContentCreate, user_id: int):
+    db_item = models.Content(**item.dict(), owner_id=user_id)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
