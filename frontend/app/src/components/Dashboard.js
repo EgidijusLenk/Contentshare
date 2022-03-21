fb24244f6a5fab0bbceca24f3e2892a12df88ba2
Egidijus Git <egidijuslenk@gmail.com>
2022-03-21 19:52:06 +0200

    work of frontend

--- frontend/app/src/components/Dashboard.js
+++ frontend/app/src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import CreateContent from './CreateContent';
 function Dashboard() {
     const [items, setItems] = useState([])
     const [user, setUser] = useState([])
@@ -12,22 +13,29 @@ function Dashboard() {
       
     function showContent(){
         console.log(headers)
-        headers["Authorization"] = `Bearer ${localStorage.getItem("token")}`
+        headers["Authorization"] =  `Bearer ${localStorage.getItem("token")}`
         axios.get("http://localhost:8000/users/me", {headers: headers})
         .then(res => { setItems(res.data.contents);setUser(res.data)})
         .catch(err => alert(`${JSON.stringify(err.response.data.detail)}`))
     }
-    useEffect(showContent, []);
+    useEffect(showContent, [CreateContent]);
     if (!items) return null;
+
+  
     const domain = window.location.hostname
     return (
         <div>
             Dashboard
             <br/>
-            <button onClick={showContent}>Show content</button>
-            <ul>x
+            {user.length >= 0 && <button onClick={showContent}>Show content</button> }
+            <br/>
+            
+              Your username: {user.user}
+            <br/>
+            <br/>
+      <CreateContent updateTable={showContent}/>
+            <ul>
                 <br/>
-               {user.user}
                {items.map((item, key) => {
                         return <li key={key}>{item.owner_id} - {item.content_url} - {domain}/{item.shortened_url}</li>
                     })}
